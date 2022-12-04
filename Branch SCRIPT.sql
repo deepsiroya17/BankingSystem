0ce71dee453d3b1512260f58001cd7e3c79d2509
akhileshkavitkar99 <kavitkar.ak@northeastern.edu>
2022-12-04 16:16:34 -0500

    Create Branch SCRIPT.sql

---
+++ Branch SCRIPT.sql
@@ -0,0 +1,5 @@
+INSERT INTO BRANCH (BRANCH_ID, NAME, STREET, CITY, STATE, ZIPCODE) VALUES (1, 'Melvin Branch', 'Melvin', 'Hartford', 'Connecticut', '06118');
+INSERT INTO BRANCH (BRANCH_ID, NAME, STREET, CITY, STATE, ZIPCODE) VALUES (2, 'High Crossing Branch', 'High Crossing', 'Kansas City', 'Kansas', '67502');
+INSERT INTO BRANCH (BRANCH_ID, NAME, STREET, CITY, STATE, ZIPCODE) VALUES (3, 'Doe Crossing Branch', 'Doe Crossing', 'Anaheim', 'California', '90401');
+INSERT INTO BRANCH (BRANCH_ID, NAME, STREET, CITY, STATE, ZIPCODE) VALUES (4, 'Fisk Branch', 'Fisk', 'Reston', 'Virginia', '20194');
+INSERT INTO BRANCH (BRANCH_ID, NAME, STREET, CITY, STATE, ZIPCODE) VALUES (5, 'Jamaica Plain Branch', 'Jamaica Plain', 'Boston', 'Massachusetts', '02130');
