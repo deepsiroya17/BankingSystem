fb9417829b7f0b26a2184aaaa589985a1f5a3cf3
Samruddha Mohire <113571876+MohireSamruddha@users.noreply.github.com>
2022-12-07 20:02:14 -0500

    Transaction update

---
+++ Transaction.sql
@@ -0,0 +1,25 @@
+    trans_id	acc_no	type	        trans_date	amount	debit_credit_flag
+        9377	785694	Cheque	        05-12-2022	2111	Credit
+        5332	651652	Cheque	        05-12-2022	3124	Credit
+        3935	678008	Cheque	        06-12-2022	7594	Credit
+        2026	928095	Zelle	        05-12-2022	6416	Credit
+        8506	220752	Cheque	        05-12-2022	1849	Credit
+        9697	570740	Zelle	        05-12-2022	510	    Credit
+        1952	724398	Zelle	        05-12-2022	4734	Credit
+        8922	703976	Demand Draft	06-12-2022	9741	Credit
+        4586	470974	Demand Draft	05-12-2022	4380	Credit
+        3871	457260	Demand Draft	05-12-2022	9819	Credit
+        9377	706041	Cheque	        05-12-2022	2111	Debit
+        5332	986242	Cheque	        05-12-2022	3124	Debit
+        3935	969223	Cheque	        06-12-2022	7594	Debit
+        2026	772314	Zelle	        05-12-2022	6416	Debit
+        8506	264075	Cheque	        05-12-2022	1849	Debit
+        9697	516916	Zelle	        05-12-2022	510	    Debit
+        1952	757487	Zelle 	        05-12-2022	4734	Debit
+        8922	207808	Demand Draft	06-12-2022	9741	Debit
+        4586	102619	Demand Draft	05-12-2022	4380	Debit
+        3871	293800	Demand Draft	05-12-2022	9819	Debit
+
+
+
+
